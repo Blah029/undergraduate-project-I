6d35022bc61793780a724829d97e38235caf05cb
Blah029 <81973454+Blah029@users.noreply.github.com>
2023-05-20 18:49:16 +0530

    Update angleCalculation.py
    - change variable name to comply with PEP8
    - create demo function

--- python/angleCalculation.py
+++ python/angleCalculation.py
@@ -3,17 +3,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def angleCalculation_v1(roadOverlay, frameSize):
+def calculate_angle_v1(roadoverlay:np.ndarray, rframesize:np.ndarray):
+    """Calculate the angle between detected road trajectory and centerline
+    of the frame and return a tuple containing thetrajectory as 
+    a 2d array of 2 points, and the angle
+    """
     # Unpack x coordinates
-    x0 = roadOverlay[0,0]
-    x1 = roadOverlay[1,0]
-    x2 = roadOverlay[2,0]
-    x3 = roadOverlay[3,0]
+    x0 = roadoverlay[0,0]
+    x1 = roadoverlay[1,0]
+    x2 = roadoverlay[2,0]
+    x3 = roadoverlay[3,0]
     # Unpack y coordinates
-    y0 = max(roadOverlay[0,1],roadOverlay[3,1])
-    y1 = min(roadOverlay[1,1],roadOverlay[2,1])
+    y0 = max(roadoverlay[0,1],roadoverlay[3,1])
+    y1 = min(roadoverlay[1,1],roadoverlay[2,1])
     # Road trajectory x coordinates
-    xt0 = 0.5*(0 + frameSize[0])   # viewport midpoint
+    xt0 = 0.5*(0 + rframesize[0])   # viewport midpoint
     xt1 = (xt0*(x1-x2) - x1*x3 + x0*x2)/(x0-x3)
     # Angle before perspective adjustment 
     angle = math.acos((y1-y0)/math.sqrt(pow(xt1-xt0, 2) + pow(y1-y0, 2)))
@@ -22,13 +26,18 @@ def angleCalculation_v1(roadOverlay, frameSize):
     return np.array([[xt0, y0], [xt1, y1]]), math.degrees(angle)
 
 
-def angleCalculation_v2(roadOverlay, frameSize):
+def calculate_angle_v2(roadoverlay:np.ndarray, rframesize:np.ndarray):
+    """Calculate the angle between detected road trajectory and centerline
+    of the frame and return a tuple containing thetrajectory as 
+    a 2d array of 2 points, and the angle
+    Further improved to be compatible with imperfect detections
+    """
     # Unpack coordinates
-    x0,y0 = roadOverlay[0,0],roadOverlay[0,1]
-    x1,y1 = roadOverlay[1,0],roadOverlay[1,1]
-    x2,y2 = roadOverlay[2,0],roadOverlay[2,1]
-    x3,y3 = roadOverlay[3,0],roadOverlay[3,1]
-    xf,yf = frameSize[0],frameSize[1]
+    x0,y0 = roadoverlay[0,0],roadoverlay[0,1]
+    x1,y1 = roadoverlay[1,0],roadoverlay[1,1]
+    x2,y2 = roadoverlay[2,0],roadoverlay[2,1]
+    x3,y3 = roadoverlay[3,0],roadoverlay[3,1]
+    xf,yf = rframesize[0],rframesize[1]
     # Calculate coordinates
     xt0 = xf/2
     yt0 = 0
@@ -52,15 +61,15 @@ def angleCalculation_v2(roadOverlay, frameSize):
     return np.array([[xt0, yt0], [xt1, yt1]]), math.degrees(angle)
 
 
-def plotOverlay(roadOverlay, frameSize,
-                trajectory, trajectoryColour="tab:orange"):
+def plot_overlay(roadoverlay:np.ndarray, rframesize:np.ndarray,
+                trajectory:np.ndarray, trajectoryColour="tab:orange"):
     # Place frame boundary
-    plt.plot(frameSize[0],frameSize[1],".", markersize=0)
+    plt.plot(rframesize[0],rframesize[1],".", markersize=0)
     # Plot detected road
-    xLeftLine = np.array([roadOverlay[0,0], roadOverlay[1,0]])
-    yLeftLine = np.array([roadOverlay[0,1], roadOverlay[1,1]])
-    xRightLine = np.array([roadOverlay[2,0], roadOverlay[3,0]])
-    yRightLine = np.array([roadOverlay[2,1], roadOverlay[3,1]])
+    xLeftLine = np.array([roadoverlay[0,0], roadoverlay[1,0]])
+    yLeftLine = np.array([roadoverlay[0,1], roadoverlay[1,1]])
+    xRightLine = np.array([roadoverlay[2,0], roadoverlay[3,0]])
+    yRightLine = np.array([roadoverlay[2,1], roadoverlay[3,1]])
     plt.plot(xLeftLine,yLeftLine,".",  markersize=0,
              linestyle="-", color="tab:blue")
     plt.plot(xRightLine,yRightLine,".",  markersize=0,
@@ -74,41 +83,46 @@ def plotOverlay(roadOverlay, frameSize,
     return trajectoryPlot
 
 
-if __name__ == "__main__":
+def demo():
+    """Demonstate angel calculation and trajectory detection"""
     # Test perfect detection
     plt.figure("Figure 01: Perfectly Detected Road")
     handles = []
     # Coordinates detected from cv
-    roadOverlay = np.array([[5,0], [10,15], [20,15], [30,0]])   # trapezium
-    frameSize = np.array([40,30])   # point
+    roadoverlay = np.array([[5,0], [10,15], [20,15], [30,0]])   # trapezium
+    rframesize = np.array([40,30])   # point
     # Version 1
-    trajectory, angle = angleCalculation_v1(roadOverlay,frameSize)
-    print(f"perfect detection   - angleCalculation_v1 = {angle}")
-    handles.append(plotOverlay(roadOverlay,frameSize,trajectory))
+    trajectory, angle = calculate_angle_v1(roadoverlay,rframesize)
+    print(f"perfect detection   - calculate_angle_v1 = {angle}")
+    handles.append(plot_overlay(roadoverlay,rframesize,trajectory))
     # Version 2
-    trajectory, angle = angleCalculation_v2(roadOverlay,frameSize)
-    print(f"perfect detection   - angleCalculation_v2 = {angle}")
-    handles.append(plotOverlay(roadOverlay,frameSize,trajectory,\
+    trajectory, angle = calculate_angle_v2(roadoverlay,rframesize)
+    print(f"perfect detection   - calculate_angle_v2 = {angle}")
+    handles.append(plot_overlay(roadoverlay,rframesize,trajectory,\
                                "tab:green"))
 
-    plt.legend(handles,["angleCalculation_v1", "angleCalculation_v2"])
+    plt.legend(handles,["calculate_angle_v1", "calculate_angle_v2"])
     plt.show()
 
     # Test imperfect detection
     plt.figure("Figure 02: Imerfectly Detected Road")
     handles = []
     # Coordinates detected from cv
-    roadOverlay = np.array([[5,7], [10,15], [20,17], [30,0]])   # trapezium
-    frameSize = np.array([40,30])   # point
+    roadoverlay = np.array([[5,7], [10,15], [20,17], [30,0]])   # trapezium
+    rframesize = np.array([40,30])   # point
     # Version 1
-    trajectory, angle = angleCalculation_v1(roadOverlay,frameSize)
-    print(f"imperfect detection - angleCalculation_v1 = {angle}")
-    handles.append(plotOverlay(roadOverlay,frameSize,trajectory))
+    trajectory, angle = calculate_angle_v1(roadoverlay,rframesize)
+    print(f"imperfect detection - calculate_angle_v1 = {angle}")
+    handles.append(plot_overlay(roadoverlay,rframesize,trajectory))
     # Version 2
-    trajectory, angle = angleCalculation_v2(roadOverlay,frameSize)
-    print(f"imperfect detection - angleCalculation_v2 = {angle}")
-    handles.append(plotOverlay(roadOverlay,frameSize,trajectory,\
+    trajectory, angle = calculate_angle_v2(roadoverlay,rframesize)
+    print(f"imperfect detection - calculate_angle_v2 = {angle}")
+    handles.append(plot_overlay(roadoverlay,rframesize,trajectory,\
                                "tab:green"))
 
-    plt.legend(handles,["angleCalculation_v1", "angleCalculation_v2"])
+    plt.legend(handles,["calculate_angle_v1", "calculate_angle_v2"])
     plt.show()
+
+
+if __name__ == "__main__":
+    demo()
