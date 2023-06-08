1db2300b5cdbb9ded96360940d54da5a09f7cb02
Blah029 <81973454+Blah029@users.noreply.github.com>
2023-06-08 23:30:26 +0530

    Fix corner detection typo, upload lane detection

--- python/corner-detection.py
+++ python/corner-detection.py
@@ -8,17 +8,24 @@ from moviepy.editor import VideoFileClip
 
 ## Read image
 inputdir = "D:\\User Files\\Documents\\University\\Misc\\4th Year Work\\Final Year Project\\Outputs\\Post Processing"
-detection = plt.imread(f"{inputdir}\\frame.png")
+# src = plt.imread(f"{inputdir}\\frame_fitaverage.png")   ## 160x80
+src = plt.imread(f"{inputdir}\\frame_laneresized.png")  ## Upscaled
 ## Preprocess
-detection = 
-gray = detection[:,:,1]
+src = np.float32(np.digitize(src,[0.5,1]))
+if len(src.shape) > 2:
+    ## Make greyscale copy
+    gray = src[:,:,1]
+else:
+    gray = src
+    ## Convert geryscale to RGB
+    src = np.dstack([np.zeros_like(src), src, np.zeros_like(src)])
 gray = np.float32(gray)
-print(gray.shape)
-# dst = cv2.cornerHarris(gray,2,3,0.04)
-dst = cv2.cornerHarris(gray,50,1,0)
+# dst = cv2.cornerHarris(gray,2,3,0.04)   ## Default
+# dst = cv2.cornerHarris(gray,2,3,0.2)   ## For 160x80
+dst = cv2.cornerHarris(gray,10,31,0)     ## For upscaled
 ## Result is dilated for marking the corners, not important
 dst = cv2.dilate(dst,None)
 ## Threshold for an optimal value, it may vary depending on the image.
-detection[dst>0.1*dst.max()]=[255,0,0]
-plt.imshow(detection[:,:,0])
+src[dst>0.1*dst.max()]=[255,0,0]
+plt.imshow(src)
 plt.show()
