4cf56be4e6af0b63d67893ae989b9093bfe0ff71
Blah029 <81973454+Blah029@users.noreply.github.com>
2024-03-10 14:43:58 +0530

    update lane_detection.py and line_generation.py, create unet_comibed_model_training.ipynb
    - lane_detection
      - use line_generation.py before detecting lane
      - create Postprocessor class
      - move moving average, quantising, cropping to Postprocessor class
    
    - line_generation
      - complete

--- python/lane_detection.py
+++ python/lane_detection.py
@@ -7,6 +7,7 @@ from tensorflow import keras
 
 import angle_calculation as angle
 import corner_detection as corner
+import line_generation as line
 import retinex
 
 
@@ -15,7 +16,8 @@ class Detector:
                  confidence:float=0.5, 
                  dumpframes:bool=False, 
                  runretinex:bool=False,
-                 retinex_low_percent:int=50):
+                 retinex_low_percent:int=50,
+                 generate:bool=False):
         """Initialise Detector class containig immediate and moving average 
         probabiltiies of each pixel be part of a lane"""
         ## Store average and recent predictions
@@ -23,8 +25,10 @@ class Detector:
         self.dumpframes = dumpframes
         self.runretinex = runretinex
         self.retinex_low_percent = retinex_low_percent
+        self.generate = generate
         self.framecount = 0
         self.predection_recent = []
+        self.generation_recent = []
         self.corners_recent = []
 
     def detect(self, image:np.ndarray=None):
@@ -37,56 +41,126 @@ class Detector:
                 self.image,
                 low_percent=self.retinex_low_percent
                 )
-        ## Strip alpha channel, resize image to 160x80, and add axis
+        ## Strip alpha channel, resize image to 160x80, and add new axis
         self.image = self.image[:,:,:3]
-        self.img_resized = cv2.resize(self.image,(160, 80))
-        self.img_resized = self.img_resized[None,:,:,:]
-        ## Recent
-        prediction = model.predict(self.img_resized)[0] * 255
+        logger.debug(f"input shape: {self.image.shape}")
+        ## Skip downscaling if using line generation. It already does that.
+        if not self.generate:
+            self.img_resized = cv2.resize(self.image,(160, 80))
+            self.img_resized = self.img_resized[None,:,:,:]
+            logger.debug(f"resized input shape: {self.img_resized.shape}")
+        
+        ## Lan line generation
+        if self.generate:
+            generator = line.LineGenerator(model_unet)
+            lines = generator.generate(self.image)
+            lines_rgb = generator.stack_rgb()
+            lines_rgb_resized = cv2.resize(lines_rgb,(self.image.shape[1],
+                                                      self.image.shape[0]))
+            generation_processor.moving_average(lines)
+            generation_processor.crop()
+            generation_processor.quantise(self.confidence)
+            generation_processor.resize(self.image.shape)
+            generation_average = np.dstack((generation_processor.average,
+                                             generation_processor.average,
+                                             generation_processor.average))
+            generation_average = generation_average[None,:,:,:]
+            logger.debug(f"lines shape: {generation_average.shape}")
+            logger.debug(f"resized shape: {generator.img_resized.shape}")
+            ## Overlay generated lines
+            self.img_resized = cv2.addWeighted(generator.img_resized.shape,1,
+                                               generation_average,1,
+                                               0, dtype=cv2.CV_32F)
+            logger.debug(f"lines rgb resized shape: {lines_rgb_resized.shape}")
+            self.image = cv2.addWeighted(self.image,1,
+                                         lines_rgb_resized,0.75,
+                                         0, dtype=cv2.CV_32F)
+        ## ==============================
+        # ## Generate lane lines
+        # if self.generate:
+        #     generator = line.LineGenerator(model_unet)
+        #     # generator.generate(self.image)
+        #     # generator.resize()
+        #     # generator.stack_rgb()
+        #     ## Recent generation
+        #     lines = generator.generate(self.image)
+        #     self.generation_recent.append(lines)
+        #     ## Moving average generation
+        #     if len(self.generation_recent) > 5:
+        #         self.generation_recent = self.generation_recent[1:]
+        #     self.generation_average = \
+        #         np.mean(np.array([i for i in self.generation_recent]), 
+        #                 axis = 0)
+        #     ## Quantise generation
+
+        #     # self.lines = generator.prediction_rgb
+        #     # self.lines_resized = generator.prediction_resized_rgb
+        #     # ## Add axis
+        #     # self.lines = self.lines[None,:,:,:]
+        #     # logger.debug(f"generated image shape: {self.lines.shape}")
+        #     # logger.debug(f"resized generated image shape: {self.lines_resized.shape}")
+        #     # ## Overlay generated lines (visualised output is not affected)
+        #     # self.img_resized = cv2.addWeighted(generator.img_resized,1,
+        #     #                                    self.lines,1,
+        #     #                                    0, dtype=cv2.CV_32F)
+        #     # self.image = cv2.addWeighted(self.image,1,
+        #     #                              self.lines_resized,1,
+        #     #                              0, dtype=cv2.CV_32F)
+        ## ==============================
+        ## Lane detection
+        prediction = model_fcnn.predict(self.img_resized)[0] * 255
         logger.debug(f"predection max: {np.max(prediction)}")
-        self.predection_recent.append(prediction)
-        ## Moving average
-        if len(self.predection_recent) > 5:
-            self.predection_recent = self.predection_recent[1:]
-        self.prediction_average = \
-            np.mean(np.array([i for i in self.predection_recent]), axis = 0)
-        logger.debug(f"predection_average max: {np.max(self.prediction_average)}, shape: {self.prediction_average.shape}")
-        ## Crop
-        self.prediction_average[:40,:] = \
-            np.zeros_like(self.prediction_average[:40,:])
-        self.prediction_average[60:,:] = \
-            np.zeros_like(self.prediction_average[60:,:])
-        ## Quantise
-        self.prediction_average = \
-            np.digitize(self.prediction_average,
-                        [self.confidence*np.max(self.prediction_average)])*255.0
-        logger.debug(f"self.prediction_average max after quantising: {np.max(self.prediction_average)}")
-        ## Resize
-        predection_resized = \
-            cv2.resize(self.prediction_average,(self.image.shape[1], 
-                                                self.image.shape[0]))
-        logger.debug(f"predection_resized max: {np.max(predection_resized)}")
-        ## Quantise
+        prediction_processor.moving_average(prediction)
+        prediction_processor.crop()
+        prediction_processor.quantise(self.confidence)
+        prediction_processor.resize(self.image.shape)
+        blanks = np.zeros_like(prediction_processor.resized).astype(np.uint8)
+        self.prediction_average = prediction_processor.average
+        self.predection_rgb = np.dstack((blanks,
+                                         prediction_processor.resized,
+                                         blanks))
+        ## ============================== 
+        # ## Recent detection
+        # self.predection_recent.append(prediction)
+        # ## Moving average detection
+        # if len(self.predection_recent) > 5:
+        #     self.predection_recent = self.predection_recent[1:]
+        # self.prediction_average = \
+        #     np.mean(np.array([i for i in self.predection_recent]), axis = 0)
+        # logger.debug(f"predection_average max: {np.max(self.prediction_average)}, shape: {self.prediction_average.shape}")
+        # ## Crop detection
+        # self.prediction_average[:40,:] = \
+        #     np.zeros_like(self.prediction_average[:40,:])
+        # self.prediction_average[60:,:] = \
+        #     np.zeros_like(self.prediction_average[60:,:])
+        # ## Quantise detection
+        # self.prediction_average = \
+        #     np.digitize(self.prediction_average,
+        #                 [self.confidence*np.max(self.prediction_average)])*255.0
+        # logger.debug(f"self.prediction_average max after quantising: {np.max(self.prediction_average)}")
+        # ## Resize
         # predection_resized = \
-        #     np.digitize(predection_resized,
-        #                 [self.confidence*np.max(predection_resized)])*255
-        # logger.debug(f"predection_resized max after quantising: {np.max(predection_resized)}")
+        #     cv2.resize(self.prediction_average,(self.image.shape[1], 
+        #                                         self.image.shape[0]))
+        # logger.debug(f"predection_resized max: {np.max(predection_resized)}")
         ## Create RGB image
-        blanks = np.zeros_like(predection_resized).astype(np.uint8)
-        self.predection_rgb = np.dstack((blanks, predection_resized, blanks))
+        # blanks = np.zeros_like(predection_resized).astype(np.uint8)
+        # self.predection_rgb = np.dstack((blanks, predection_resized, blanks))
+        ## ==============================
         ## Dump detection
         if self.dumpframes == True:
             dir_dump = "C:\\Users\\User Files\\Documents\\University\\Misc\\4th Year Work\\Final Year Project\\Outputs\\Model Outputs\\framedump"
             cv2.imwrite(f"{dir_dump}\\{self.framecount}-1_average.png",self.prediction_average)
-            cv2.imwrite(f"{dir_dump}\\{self.framecount}-2_resized.png",predection_resized)
+            cv2.imwrite(f"{dir_dump}\\{self.framecount}-2_resized.png",prediction_processor.resized)
             self.framecount += 1
 
-    def overlay_lane(self,image):
+    def overlay_lane(self, image:np.ndarray):
         """Overlay detection onto the image"""
         self.image = image
         self.detect()
-        self.result = cv2.addWeighted(self.image,1,self.predection_rgb,1,0, 
-                                              dtype=cv2.CV_32F)
+        self.result = cv2.addWeighted(self.image,1,
+                                      self.predection_rgb,1,
+                                      0, dtype=cv2.CV_32F)
         self.result = np.clip(self.result,None,255)
         ## Dump result
         if self.dumpframes == True:
@@ -94,13 +168,13 @@ class Detector:
             cv2.imwrite(f"{dir_dump}\\{self.framecount-1}-3_laneimage.png",self.result[:,:,::-1])
         return self.result
     
-    def replace_lane(self,image):
+    def replace_lane(self, image:np.ndarray):
         """Replace image with lane detection mask"""
         self.image = image
         self.detect()
         return self.predection_rgb
     
-    def overlay_all(self,image):
+    def overlay_all(self, image:np.ndarray):
         """Overlay lane detection, angle deviation, positional deviation and 
         departure warning onto the image
         """
@@ -126,21 +200,68 @@ class Detector:
             cv2.imwrite(f"{dir_dump}\\{self.framecount-1}-4_trajectoryimage.png",self.result[:,:,::-1])
         return self.result
 
+
+class Postprocessor:
+    def __init__(self, 
+                 average_count:int=5):
+        """Initialise postprocessor class for taking moving average, cropping,
+        and upscaling binary mask of 0-255 range"""
+        self.average_count = average_count
+        self.recent = []
+        
+    def moving_average(self, mask:np.ndarray):
+        """Take moving average of specified number of masks"""
+        self.recent.append(mask)
+        logger.debug(f"mask max: {np.max(mask)}")
+        logger.debug(f"recents lenght: {len(self.recent)}")
+        if len(self.recent) > self.average_count:
+            self.recent = self.recent[1:]
+        self.average = \
+            np.mean(np.array([i for i in self.recent]), axis = 0)
+        logger.debug(f"average max: {np.max(self.average)}, shape: {self.average.shape}")
+
+    def crop(self, bottom:int=40, top:int=60):
+        """Set elements below the specified bottom row and above the 
+        specified top row to 0"""
+        self.average[:bottom,:] = \
+            np.zeros_like(self.average[:bottom,:])
+        self.average[top:,:] = \
+            np.zeros_like(self.average[top:,:])
+        
+    def quantise(self, confidence:float=0.5):
+        """Quantise mask to 0 and 1"""
+        self.average = \
+            np.digitize(self.average,
+                        [confidence*np.max(self.average)])*255.0
+        logger.debug(f"average max after quantising: {np.max(self.average)}")
+
+    def resize(self, resize_dimensions:tuple=None):
+        """Resize mask"""
+        self.resize_width = resize_dimensions[1]
+        self.resize_height = resize_dimensions[0]
+        self.resized = \
+            cv2.resize(self.average,(self.resize_width,self.resize_height))
+        logger.debug(f"resized max: {np.max(self.resized)}")
+        
+
+
 ## Set up the logger
 logging.basicConfig(format="[%(name)s][%(levelname)s] %(message)s")
 logger = logging.getLogger("corner-detection")
 ## Script
 if __name__ == "__main__":
     # logger.setLevel(logging.DEBUG)
+    # line.logger.setLevel(logging.DEBUG)
     ## Set I/O
-    dir_model = "C:\\Users\\User Files\\Documents\\University\\Misc\\4th Year Work\\Final Year Project\\Models\\FCNN"
+    dir_model_fcnn = "C:\\Users\\User Files\\Documents\\University\\Misc\\4th Year Work\\Final Year Project\\Models\\FCNN"
+    dir_model_unet = "C:\\Users\\User Files\\Documents\\University\\Misc\\4th Year Work\\Final Year Project\\Models\\UNet\\Combined"
     # dir_input = "C:\\Users\\User Files\\Documents\\University\\Misc\\4th Year Work\\Final Year Project\\Datasets\\video-footage\\local-dashcam"
     dir_input = "C:\\Users\\User Files\\Documents\\University\\Misc\\4th Year Work\\Final Year Project\\Datasets\\video-footage"
     dir_output = "C:\\Users\\User Files\\Documents\\University\\Misc\\4th Year Work\\Final Year Project\\Outputs\\Model Outputs"
     vid_name = "techodyssey_1080p.MP4"
     ## Original model
     # confidence = 0.05
-    # model = keras.models.load_model(f"{dir_model}\\MLND-Capstone\\full_CNN_model.h5")
+    # model_fcnn = keras.models.load_model(f"{dir_model_fcnn}\\MLND-Capstone\\full_CNN_model.h5")
     # label = f"originalmodel_{confidence}"
     ## Duct tape fixed model trained on custom data set of 250 images
     # model_version = 0
@@ -149,14 +270,19 @@ if __name__ == "__main__":
     model_version = 3
     confidence = 0.35     ## optimal threshold for daytime
     ## Complie separately to fix weight_decay typeerror
-    model = keras.models.load_model(f"{dir_model}\\test_model_v{model_version}.h5", compile=False)
-    model.compile(optimizer='Adam', loss='mean_squared_error')
-    label = f"custommodelv{model_version}_{confidence}_msrcr"
+    model_fcnn = keras.models.load_model(f"{dir_model_fcnn}\\test_model_v{model_version}.h5", compile=False)
+    model_fcnn.compile(optimizer='Adam', loss='mean_squared_error')
+    model_unet = keras.models.load_model(f"{dir_model_unet}\\combined_model.h5", compile=False)
+    model_unet.compile(optimizer='adam', 
+                       loss=keras.losses.BinaryFocalCrossentropy())
+    label = f"fcnn-{model_version}_conf-{confidence}_unet"
     ## Perform detection
-    detector = Detector(confidence)
-    ## Configure retinex
-    detector.runretinex = True
-    detector.retinex_low_percent = 50
+    generation_processor = Postprocessor()
+    prediction_processor = Postprocessor()
+    detector = Detector(confidence=confidence,
+                        runretinex=False,           ## Retinex enhancement
+                        retinex_low_percent=50,
+                        generate=True)             ## Generate lane lines
     vid_input = VideoFileClip(f"{dir_input}\\{vid_name}", audio=False)
         
     def detect_clip(start:int, end:int):
@@ -173,7 +299,7 @@ if __name__ == "__main__":
         
     def detect_full():
         """Perform lane detection on a full a video"""
-        detector.dumpframes = True
+        detector.dumpframes = False
         # Modify frames
         vid_output = vid_input.fl_image(detector.overlay_lane)
         # Save video
@@ -199,6 +325,6 @@ if __name__ == "__main__":
             )
         
     # detect_clip(3,6)
-    detect_full()
-    # detectncalculate_clip(14,19)
-    # detectncalculate_full()
+    # detect_full()
+    # detectncalculate_clip(14,14.17)
+    detectncalculate_full()
--- python/line_generation.py
+++ python/line_generation.py
@@ -2,6 +2,7 @@
 import cv2
 import keras
 import logging
+import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 
@@ -10,24 +11,61 @@ class LineGenerator:
     def __init__ (self, model):
         """Initialise LineGenerator class"""
         self.model = model
+        self.prediction = None
+        self.prediction_resized = None
 
     def generate(self, image:np.ndarray):
-        ## Strip alpha channel, resize image to 320x256, and add axis
-        self.image = image[:,:,:3]
-        self.img_resized = cv2.resize(self.image,(320,256))
-        self.img_resized = self.img_resized[None,:,:,:]
-        logger.debug(f"added new axis: {self.img_resized.shape}")
-        self.img_resized = self.img_resized.reshape(1,256,320,3)
-        logger.debug(f"rehsaped: {self.img_resized.shape}")
-        prediction = self.model.predict(image)
+        """Generate lane line mask for 160x80 image using unet"""
+        ## Strip alpha channel, resize image to 160x80, and add axis
+        if image.shape != (1, 80, 160, 3):
+            self.image = image[:,:,:3]
+            self.img_resized = cv2.resize(self.image,(160,80))
+            self.img_resized = self.img_resized[None,:,:,:]
+            logger.debug(f"image read: {self.image.shape}")
+            logger.debug(f"image resized: {self.img_resized.shape}")
+            logger.debug(f"added new axis: {self.img_resized.shape}")
+        else:
+            self.image = image
+            self.img_resized = image
+            logger.debug(f"image already processed to shape {image.shape}")
+        self.prediction = self.model.predict(self.img_resized)
         ## Remove negative values and quantise
-        prediction[prediction < 0] = 0.0
-        prediction = (prediction >= 0.5).astype('int')
-        prediction = prediction.squeeze()
-        self.prediction_resized = cv2.resize(prediction,)
-        return self.prediction
+        self.prediction[self.prediction < 0] = 0.0
+        self.prediction = (self.prediction >= 0.5).astype('uint8')
+        self.prediction = self.prediction.squeeze()*255
+        logger.debug(f"mask shape: {self.prediction.shape}")
+        self.output_mask = self.prediction
+        return self.output_mask
 
+    def resize(self):
+        """Rescale the output mask to match input resolution"""
+        if self.image.shape != (1, 80, 160, 3):
+            self.prediction_resized = cv2.resize(self.prediction, 
+                                                    (self.image.shape[1],
+                                                    self.image.shape[0]))
+        else:
+            self.prediction_resized = self.prediction
+        logger.debug(f"resized shape: {self.prediction_resized.shape}")
+        self.output_mask = self.prediction_resized
+        return self.output_mask
 
+    def stack_rgb(self, stack_160x80:bool=True, stack_resized:bool=True):
+        """Depth stack output mask to create RGB image"""
+        if stack_160x80:
+            self.prediction_rgb = np.dstack((self.prediction,
+                                             self.prediction,
+                                             self.prediction))
+            logger.debug(f"rgb mask shape: {self.prediction_rgb.shape}")
+            self.output_mask = self.prediction_rgb
+        if stack_resized and self.prediction_resized is not None:
+            self.prediction_resized_rgb = np.dstack((self.prediction_resized,
+                                                     self.prediction_resized,
+                                                     self.prediction_resized))
+            logger.debug(f"resized rgb mask shape: {self.prediction_resized_rgb.shape}")
+            self.output_mask = self.prediction_resized_rgb
+        return self.output_mask
+    
+    
 ## Set up the logger
 logging.basicConfig(format="[%(name)s][%(levelname)s] %(message)s")
 logger = logging.getLogger("line-generation")
@@ -35,13 +73,28 @@ logger = logging.getLogger("line-generation")
 if __name__ == "__main__":
     logger.setLevel(logging.DEBUG)
     ## Directory paths
-    dir_model = r"C:\Users\User Files\Documents\University\Misc\4th Year Work\Final Year Project\Models\UNet"
-    dir_input = r"C:\Users\User Files\Documents\University\Misc\4th Year Work\Final Year Project\Datasets\image-footage"
-    dir_output = r"C:\Users\User Files\Documents\University\Misc\4th Year Work\Final Year Project\Outputs\Model Outputs\UNet"
+    dir_model = "C:\\Users\\User Files\\Documents\\University\\Misc\\4th Year Work\\Final Year Project\\Models\\UNet"
+    dir_input = "C:\\Users\\User Files\\Documents\\University\\Misc\\4th Year Work\\Final Year Project\\Datasets\\image-footage"
+    # dir_input = "C:\\Users\\User Files\\Documents\\University\\Misc\\4th Year Work\\Final Year Project\\Datasets\\image-footage\\CULane Selected\\Image-Label Couples\\images"
+    dir_output = "C:\\Users\\User Files\\Documents\\University\\Misc\\4th Year Work\\Final Year Project\\Outputs\\Model Outputs\\UNet"
+    image = cv2.imread(f"{dir_input}\\techodyssey_1080p_1.png")
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     ## Load model
     # model = tf.keras.models.load_model(f"{dir_model}\\new_model.h5")
-    model = tf.keras.models.load_model(f"{dir_model}\\new_model.h5", compile=False)
-    model.compile(optimizer='adam', loss=keras.losses.BinaryFocalCrossentropy(), metrics=['accuracy'])
+    model = tf.keras.models.load_model(f"{dir_model}\\Combined\\combined_model.h5", compile=False)
+    model.compile(optimizer='adam', 
+                  loss=keras.losses.BinaryFocalCrossentropy(), 
+                  metrics=['accuracy'])
     generator = LineGenerator(model)
+    generator.generate(image)
+    generator.resize()
+    generator.stack_rgb()
+    fig, ax = plt.subplots(2,1)
+    ax[0].imshow(generator.image)
+    ax[0].set_title("Image")
+    ax[1].imshow(generator.output_mask)
+    ax[1].set_title("Label")
+    fig.tight_layout()
+    plt.show()
 
 
